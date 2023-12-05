5e64b1410f2c6e3a15822cb94ddaf906d5b9a362
LittleButtersome <152962462+LittleButtersome@users.noreply.github.com>
2023-12-05 16:28:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+
+
+ 
+   //Questions array
+            let questions = [
+            {
+                 number:1,
+                 question: "What defines blockchain technology?",
+                 answer : "Decentralized,Distributed Ledger System",
+                 options : [
+                                  "Centralized Ledger System",
+                                  "Decentralized,Distributed Ledger System",
+                                  "Localized Data Storage System ",
+                                  "Hierarchical Data Structure"
+                                ]
+								
+            },
+            {
+                 number:2,
+                 question: "How does Artificial Intelligence(AI)impact industries??",
+                 answer : "Automates tasks and improves efficiency",
+                 options : [
+                                  "Slows down processes",
+                                  "Reduces data analysis ",
+                                  "Automates tasks and improves efficiency",
+                                  "Limits innovation possibilities"  
+                                  ]
+          
+            },
+           {
+                 number:3,
+                 question: "What is the primary advantage of 5G technology??",
+                 answer : "Faster data transfer rates and lower latency",
+                 options : [ 
+                                  "Slower data transfer rates",
+                                  "Higher latency",
+                                  "Limited connectivity",
+                                  "Faster data transfer rates and lower latency"
+                                  ]
+            
+            },
+            {
+                 number:4,
+                 question: "How do quantum computers differ from classical computers?",
+                 answer : "They use qubits and perform exponentially faster calculations",
+                 options : [
+                                  "They use bits for calculations",
+                                  "They perform calculations at a slower pace",
+                                  "They use qubits and perform exponentially faster calculations",
+                                  "They are limited to simple computations"
+                                  ]
+								  
+            }, 
+            {
+                 number:5,
+                 question: "What does the Internet of Things (IoT) enable?",
+                 answer : "Remote monitoring, data collection, and automation",
+                 options : [ 
+                                  "Connectivity only in urban areas",
+                                  "Remote monitoring, data collection, and automation",
+                                  "Limited device compatibility",
+                                  "Exclusive use in industrial settings"
+                               ]
+							   
+            },
+            {
+                 number:6,
+                 question: "What is one of the ethical concerns in biotechnology advancements?",
+                 answer : "Potential misuse of gene editing technologies",
+                 options : [
+                                  "Lack of innovation",
+                                  "Limited use in healthcare",
+                                  "Potential misuse of gene editing technologies",
+                                  "Ethical acceptance worldwide"
+                               ]
+							   
+            },
+            {
+                 number:7,
+                 question: "How are renewable energy technologies evolving?",
+                 answer : "Through innovations in efficiency and storage",
+                 options : [
+                                  "They are becoming less efficient",
+                                  "They are becoming more expensive",
+                                  "Through innovations in efficiency and storage",
+                                  "They are losing popularity"
+                               ]
+						
+            },
+             {
+                 number:8,
+                 question: "What is a challenge in the adoption of autonomous vehicles?",
+                 answer : "Safety concerns and ethical dilemmas",
+                 options : [
+                                 "Lack of regulatory hurdles",
+                                 "High level of public acceptance",
+                                 "Safety concerns and ethical dilemmas",
+                                 "No need for infrastructure updates"
+                               ]
+							   
+			 },
+            {
+                 number:9,
+                 question: "What defines augmented reality (AR)??",
+                 answer : "Overlaying digital information onto the real world",
+                 options : [
+                                 "Replacement of reality with digital information",
+                                 "Overlaying digital information onto the real world",
+                                 "Limited applications in gaming",
+                                 "Irrelevant in the healthcare industry"
+                               ]     
+  				   
+		   },
+            {
+                 number:10,
+                 question: "How does biometric technology impact security and privacy?",
+                 answer : "Raises concerns about data privacy",
+                 options : [
+                                 "Reduces security measures",
+                                 "Raises concerns about data privacy",
+                                 "Increases data vulnerability",
+                                 "Minimizes identification accuracy"
+                               ]
+							   
+            }
+             ];    
